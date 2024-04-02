1fad892361df9392a5d66292fa2958d83749aefe
stereobooster <stereobooster@gmail.com>
2024-04-02 17:32:35 +0200

    update

--- test/index.test.js
+++ test/index.test.js
@@ -11,15 +11,25 @@ const assertEqualFiles = async (file) => {
 
 if (!existsSync("test/tmp")) mkdirSync("test/tmp");
 
-test("graph 1", async () => {
-  await vennSnap("test/data/g1.json", "test/tmp/g1.svg");
-  await assertEqualFiles("g1.svg");
+test("simple", async () => {
+  await vennSnap("test/data/simple.json", "test/tmp/simple.svg");
+  await assertEqualFiles("simple.svg");
 });
 
 test("lastfm", async () => {
   try {
     await vennSnap("test/data/lastfm.json", "test/tmp/lastfm.svg");
     await assertEqualFiles("lastfm.svg");
+  } catch (e) {
+    assert.match(e, /TypeError: tspan.getComputedTextLength is not a function/);
+    assert.fail("TypeError: tspan.getComputedTextLength is not a function");
+  }
+});
+
+test("medical", async () => {
+  try {
+    await vennSnap("test/data/medical.json", "test/tmp/medical.svg");
+    await assertEqualFiles("medical.svg");
   } catch (e) {
     assert.match(e, /TypeError: tspan.getComputedTextLength is not a function/);
     assert.fail("TypeError: tspan.getComputedTextLength is not a function");
